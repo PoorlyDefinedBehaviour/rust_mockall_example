408a2d4803a50612510f3570cae6f24dc5f402a1
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2022-02-07 19:47:42 -0300

    feat: define ports

---
+++ src/auth/ports.rs
@@ -0,0 +1,35 @@
+use async_trait::async_trait;
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
+pub struct Credentials {
+  pub username: String,
+  pub password: String
+}
+
+pub type Token = String;
+
+#[cfg_attr(test, mockall::automock)]
+#[async_trait]
+pub trait AuthService {
+  /// Registers a new user.
+  async fn register(&self, credentials: Credentials) -> bool;
+  /// User can use its credentials to get a token.
+  async fn login(&self, credentials: &Credentials) -> Option<Token>;
+  async fn authenticate(&self, token: &Token) -> Option<String>;
+}
+
+#[cfg_attr(test, mockall::automock)]
+#[async_trait]
+pub trait CredentialsRepo {
+  async fn save_credential(&self, credentials: &Credentials) -> bool;
+  async fn credential_exists(&self, credentials: &Credentials) -> bool;
+}
+
+#[cfg_attr(test, mockall::automock)]
+#[async_trait]
+pub trait TokenRepo {
+  async fn generate_token(&self) -> Token;
+  async fn save_token(&self, token: &Token,username:&String)->bool;
+  async fn get_username_by_token(&self, token: &Token) -> Option<String>;
+}
